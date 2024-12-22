71ef4d82d9756f4b0cc6d9d393171578d7d81a97
Yuuri Kurashima <kurashimayuuri@gmail.com>
2024-12-22 13:35:16 +0900

    test

--- frontend/src/lib/tailwindcss/index.ts
+++
@@ -1,7 +0,0 @@
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-
-export function tw(...inputs: ClassValue[]): string {
-    return twMerge(clsx(inputs));
-}
